1013201af271ebb579904ee9eb6a6c5204b887dd
lazycoder1 <gautamgsabhahit@gmail.com>
2023-09-05 00:52:26 +0530

    fixing SEO

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -4,20 +4,20 @@ import Link from 'next/link';
 
 const SEO = () => (
     <Head>
-        <meta name="description" content="User Operation explorer for Account Abstraction (ERC-4337)" />
-        <meta name="keywords" content="4337, Account Abstraction, Explorer, BlockChain, Paymaster, Bundler, Entrypoint"></meta>
+        <meta name="description" content="Safe Ecosystem Explorer !" />
+        <meta name="keywords" content="Safe, Account Abstraction, Explorer, BlockChain"></meta>
         <meta name="author" content="Jiffy Labs"></meta>
-        <meta property="og:title" content="Jiffy Scan - User Op Explorer" />
+        <meta property="og:title" content="Safe Scan - Safe Explorer" />
         <meta property="og:type" content="blockchain explorer" />
-        <meta property="og:url" content="https://www.jiffyscan.xyz/" />
+        {/* <meta property="og:url" content="https://www.jiffyscan.xyz/" /> */}
         <meta property="og:image" content="https://jiffyscan-public-assets.s3.us-east-2.amazonaws.com/favicon.jpg" />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="twitter:site" content="@jiffylabs"></meta>
         <meta name="twitter:creator" content="@jiffylabs"></meta>
-        <meta name="twitter:title" content="Jiffy Scan - User Op Explorer"></meta>
-        <meta name="twitter:description" content="User Operation explorer for Account Abstraction (ERC-4337)"></meta>
+        <meta name="twitter:title" content="Safe Scan - Safe ecosystem Explorer"></meta>
+        <meta name="twitter:description" content="Transaction Explorer for safe ecosystem"></meta>
         <meta name="twitter:image" content="https://jiffyscan-public-assets.s3.us-east-2.amazonaws.com/favicon.jpg"></meta>
-        <title>Jiffy Scan - User Op Explorer</title>
+        <title>Safe Scan - Safe Ecosystem Explorer</title>
     </Head>
 );
 
